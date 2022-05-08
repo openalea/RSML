bdabd1ca359e84f44ca8f4ab29ea752f284111d7
pradal <christophe.pradal@inria.fr>
2022-05-08 22:12:03 +0200

    Add conda packaging and remove appveyor and travis CI

--- setup.py
+++ setup.py
@@ -4,28 +4,29 @@ __revision__ = "$Id: $"
 import sys
 
 from setuptools import setup, find_packages
-from openalea.deploy.metainfo import read_metainfo
 
-# Reads the metainfo file
-metadata = read_metainfo('metainfo.ini', verbose=True)
-for key, value in metadata.iteritems():
-    exec("%s = '%s'" % (key, value))
+# Define metainfo
+version = '1.1.1'
+name = 'RSML'
+package = 'RSML'
+description= 'RSML package provide IO functionality between .rsml file and MTG'
+long_description=  'RSML package provide IO functionality between .rsml file and MTG, as well as ploting and standard measurements.' 
+authors= 'C. Pradal, J. Diener'
+authors_email = 'christophe.pradal@inria.fr'
+url = 'https://github.com/openalea/RSML'
+license = 'Cecill-C'
+
 
 # Packages list, namespace and root directory of packages
 
-pkgs = [pkg for pkg in find_packages('src')]
+pkgs = find_packages('src')
 packages = pkgs
 package_dir = dict([('', 'src')] + [(pkg, "src/" + pkg) for pkg in pkgs])
 
 # dependencies to other eggs
 setup_requires = ['openalea.deploy']
-if("win32" in sys.platform):
-    install_requires = []
-else:
-    install_requires = []
 
 # web sites where to find eggs
-dependency_links = ['http://openalea.gforge.inria.fr/pi']
 setup(
     name=name,
     version=version,
@@ -42,16 +43,10 @@ setup(
     package_dir=package_dir,
 
     # Namespace packages creation by deploy
-    #namespace_packages = [namespace],
-    #create_namespaces = False,
     zip_safe=False,
 
     # Dependencies
     setup_requires=setup_requires,
-    install_requires=install_requires,
-    dependency_links=dependency_links,
-
-
 
     # Eventually include data in your package
     # (folowing is to include all versioned files other than .py)
