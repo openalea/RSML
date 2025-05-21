ca37bf7d814838193890665ed88ebf48b84e09bc
baugetfa <fabrice.bauget@cirad.fr>
2025-05-21 15:57:17 +0200

    use of SETUPTOOLS_SCM_PRETEND_VERSION in conda.yaml, gh action according to core

--- src/rsml/version.py
+++ src/rsml/version.py
@@ -1,13 +1,3 @@
-# copied from openalea.mtg
-import importlib.metadata
 
-__version__ =  importlib.metadata.version("rmsl")
-
-numbers = __version__.split(".")
-
-try:
-    major, minor, post = numbers[0], numbers[1], numbers[2]
-    patch = '.'.join(numbers[3:])
-except IndexError:
-    major, minor, post = '1', '4', '0'
-    patch = '0'
+major, minor, post = '1', '4', '0'
+__version__ = '.'.join([major, minor, post])
